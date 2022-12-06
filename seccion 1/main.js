6d8b562b65fb5da9e8608804b7d748fcf250b1c2
camilohm1 <cahernandez3510@misena.edu.co>
2022-12-06 08:30:40 -0500

    seccion 1 punto 6

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -2,21 +2,18 @@ addEventListener("DOMContentLoaded" ,(e)=>{
     let form=document.querySelector('#input')
     let cont =1   
     form.addEventListener("submit",(e)=>{
-        e.preventDefault();    
-        if(cont <=5){
-            let datosEntrada = Object.fromEntries(new FormData(e.target))
-            let observaciones= datosEntrada.observaciones
-            let hora= (datosEntrada.hora)
-
-            let tabla=document.querySelector("tbody");
-                tabla.insertAdjacentHTML("beforeend",`
-                <tr>
-                <td> ${cont}  </td>
-                        <td> ${observaciones}  </td>
-                        <td> ${hora} </td>
-                    </tr>
-                    `)
-                    cont++
-                }
-            })  
+        e.preventDefault();       
+        if(cont <=50){
+        let datosEntrada = Object.fromEntries(new FormData(e.target))
+        let observaciones= datosEntrada.observaciones
+                let tabla=document.querySelector("tbody");
+            tabla.insertAdjacentHTML("beforeend",`
+            <tr>
+                    <td> ${cont}  </td>
+                    <td> ${observaciones}  </td>
+                </tr>
+                `)
+            cont++
+        }
+    })  
 })
