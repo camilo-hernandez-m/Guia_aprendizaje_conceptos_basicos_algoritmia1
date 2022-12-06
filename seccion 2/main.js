51371eb16dee4b9e34486e167dbaf58d7f016440
camilohm1 <cahernandez3510@misena.edu.co>
2022-12-06 10:32:53 -0500

    seccion 2 punto 2

--- seccion 2/main.js
+++ seccion 2/main.js
@@ -1,29 +1,18 @@
 addEventListener("DOMContentLoaded" ,(e)=>{
-    let form=document.querySelector('#input')
-    let cont =1   
-    form.addEventListener("submit",(e)=>{
-        e.preventDefault();       
-
-        if(cont <=50){
-
-    let datosEntrada = Object.fromEntries(new FormData(e.target))
-    let distancia=Number(42.195)
-        let tiempo= Number(datosEntrada.tiempo)
-            let res= (distancia/tiempo)
-            let res2=   (res*0.001)
-            let res4=res2.toFixed(2)
-            let res3=res.toFixed(2)
+let form=document.querySelector('#input')
+form.addEventListener("submit",(e)=>{
+        let datosEntrada=Object.fromEntries(new FormData(e.target))
+        e.preventDefault(); 
+        let c=datosEntrada.ciudad    
+        let f=Number(datosEntrada.Fahrenheit) 
+        let res=(f =9/5)  + 32
         let tabla=document.querySelector("tbody")
         tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-            <td> ${cont}  </td>
-                <td> ${distancia} KM </td>
-                <td> ${tiempo} H </td>
-                <td> ${res3}  M </td>
-                <td> ${res4}  KM </td>
-            </tr>
-            `)
-        cont++
-        }
-    })
+        <tr>
+            <td>${c} </td>
+            <td>${f} </td>
+            <td>${res}  </td>
+        </tr>
+        `)
+    }) 
 })
