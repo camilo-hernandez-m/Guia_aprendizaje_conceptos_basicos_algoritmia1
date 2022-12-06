57c8c7b7c12890d2750abd603f3fd56cad0c339d
camilohm1 <cahernandez3510@misena.edu.co>
2022-12-06 08:25:29 -0500

    seccion 1 punto 5

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -1,18 +1,22 @@
-addEventListener("DOMContentLoaded"),(e)=>{
+addEventListener("DOMContentLoaded" ,(e)=>{
     let form=document.querySelector('#input')
-        form.addEventListener("submit",(e)=>{
-            let datosEntrada=Object.fromEntries(new FormData(e.target))
-            e.preventDefault(); 
-            let c=datosEntrada.ciudad    
-            let f=Number(datosEntrada.Fahrenheit) 
-            let res=(f *9/5) + 32 
-            let tabla=document.querySelector("tbody")
-            tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-                <td>${c} </td>
-                <td>${f} </td>
-                <td>${res}  </td>
-            </tr>
-            `)
-        }) 
-}
+    let cont =1   
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();    
+        if(cont <=5){
+            let datosEntrada = Object.fromEntries(new FormData(e.target))
+            let observaciones= datosEntrada.observaciones
+            let hora= (datosEntrada.hora)
+
+            let tabla=document.querySelector("tbody");
+                tabla.insertAdjacentHTML("beforeend",`
+                <tr>
+                <td> ${cont}  </td>
+                        <td> ${observaciones}  </td>
+                        <td> ${hora} </td>
+                    </tr>
+                    `)
+                    cont++
+                }
+            })  
+})
