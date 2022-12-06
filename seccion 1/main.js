d26e62dce96ec194c3db0b75403cfbd37c008b8f
camilohm1 <cahernandez3510@misena.edu.co>
2022-12-06 08:12:29 -0500

    seccion 1 punto 2

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -1,3 +1,24 @@
 addEventListener("DOMContentLoaded"),(e)=>{
-    
+    function conjuncion()
+    {
+        // para obetener el valor//
+        var conj1 = document.getElementById("conjuncion1").value;
+        var conj2 = document.getElementById("conjuncion2").value;
+        if(conj1 == "v" && conj2 == "v"){
+            document.getElementById("res").value="verdadero";
+        }else if((conj1 == "v" && conj2 == "f") || (conj1 =="f" && conj2 == "v") || (conj1 == "f" && conj2 == "f")){
+            document.getElementById("res").value = "falso";
+        }
+    }
+    function disyuncion()
+    {
+        // para obetener el valor//
+        var dis1 = document.getElementById("disyuncion1").value;
+        var dis2 = document.getElementById("disyuncion2").value;
+        if(dis1 == "f" && dis2 == "f"){
+            document.getElementById("resdis").value="falso";
+        }else if((dis1 == "v" && dis2 == "v") || (dis1 =="v" && dis2 == "f") || (dis1 == "f" && dis2 == "v")){
+            document.getElementById("resdis").value = "verdadero";
+        }
+    }
 }
