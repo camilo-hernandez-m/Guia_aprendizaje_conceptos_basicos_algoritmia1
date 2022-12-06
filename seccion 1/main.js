35d936f4667df44ccca333ccdb06d9f2180d8a78
camilohm1 <cahernandez3510@misena.edu.co>
2022-12-06 08:15:58 -0500

    seccion 1 punto 4

--- seccion 1/main.js
+++ seccion 1/main.js
@@ -1,11 +1,18 @@
 addEventListener("DOMContentLoaded"),(e)=>{
-    function cambio(){
-        let dol = 4800; 
-        let re ;   
-        let pe =parseInt(document.getElementById('pesos').value, 10);
-         
-        re = pe / dol ;
-        document.getElementById('resul').innerHTML= re;
-        
-        }
+    let form=document.querySelector('#input')
+        form.addEventListener("submit",(e)=>{
+            let datosEntrada=Object.fromEntries(new FormData(e.target))
+            e.preventDefault(); 
+            let c=datosEntrada.ciudad    
+            let f=Number(datosEntrada.Fahrenheit) 
+            let res=(f *9/5) + 32 
+            let tabla=document.querySelector("tbody")
+            tabla.insertAdjacentHTML("beforeend",`
+            <tr>
+                <td>${c} </td>
+                <td>${f} </td>
+                <td>${res}  </td>
+            </tr>
+            `)
+        }) 
 }
