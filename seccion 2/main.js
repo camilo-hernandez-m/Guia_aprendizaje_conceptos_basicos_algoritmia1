9dab29f32a3d67f3f065310fb8c65d55256ffb7c
camilohm1 <cahernandez3510@misena.edu.co>
2022-12-06 10:29:54 -0500

    seccion 2 punto 1

--- seccion 2/main.js
+++ seccion 2/main.js
@@ -3,54 +3,27 @@ addEventListener("DOMContentLoaded" ,(e)=>{
     let cont =1   
     form.addEventListener("submit",(e)=>{
         e.preventDefault();       
+
         if(cont <=50){
-        let datosEntrada = Object.fromEntries(new FormData(e.target))
-    let año=datosEntrada.año
-    // let res= año/4
-    if (año % 4 != 0){
-        let tabla=document.querySelector("tbody");
+
+    let datosEntrada = Object.fromEntries(new FormData(e.target))
+    let distancia=Number(42.195)
+        let tiempo= Number(datosEntrada.tiempo)
+            let res= (distancia/tiempo)
+            let res2=   (res*0.001)
+            let res4=res2.toFixed(2)
+            let res3=res.toFixed(2)
+        let tabla=document.querySelector("tbody")
         tabla.insertAdjacentHTML("beforeend",`
             <tr>
-                <td>${año}  </td>
-                <td>  </td>
-                <td> X  </td>
+            <td> ${cont}  </td>
+                <td> ${distancia} KM </td>
+                <td> ${tiempo} H </td>
+                <td> ${res3}  M </td>
+                <td> ${res4}  KM </td>
             </tr>
             `)
-    }
-
-
-    else if (año % 4 == 0 & año % 100 != 0){
-        let tabla=document.querySelector("tbody");
-        tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-                <td>${año}  </td>
-                <td> X </td>
-                <td>   </td>
-            </tr>
-            `)
-    } 
-    else if(año % 4 == 0 & año % 100 == 0 & año % 400 != 0){
-        let tabla=document.querySelector("tbody");
-        tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-                <td>${año}  </td>
-                <td>  </td>
-                <td> X  </td>
-            </tr>
-            `)
-    } 
-
-
-    else if(año % 4 == 0 & año % 100 == 0 & año % 400 == 0){
-        let tabla=document.querySelector("tbody");
-        tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-                <td>${año}  </td>
-                <td>  X </td>
-                <td>   </td>
-            </tr>
-            `)
-            } 
+        cont++
         }
     })
 })
